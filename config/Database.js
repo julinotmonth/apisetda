9c7d6966763b3f82ca63df8839ef7c8bd13c3eb6
jeje <akhmdjzl17@gmail.com>
2025-02-18 23:34:11 +0700

    first committ

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,18 @@
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const db = new Sequelize('setda','root','',{
-    host: 'localhost',
-    dialect: 'mysql'
-});
+dotenv.config();
+
+const db = new Sequelize(
+    process.env.DB_NAME, // Nama database
+    process.env.DB_USER, // Username database
+    process.env.DB_PASSWORD, // Password database
+    {
+        host: process.env.DB_HOST || "localhost",
+        dialect: process.env.DB_DIALECT || "mysql",
+        port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
+        logging: false, // Matikan logging jika tidak diperlukan
+    }
+);
 
 export default db;
