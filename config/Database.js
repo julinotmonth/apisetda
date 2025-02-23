fdc48b93a8e9136365f802847e17f2822ed365ee
jeje <akhmdjzl17@gmail.com>
2025-02-23 13:40:12 +0700

    tes

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,10 @@
 import {Sequelize} from "sequelize";
+import mysql2 from "mysql2";
 
 const db = new Sequelize('bhhobmavjrngkbkydgl6','u9h0ifiima73m3tt','YcGhnWsrUYUfsPfvmboe',{
     host: 'bhhobmavjrngkbkydgl6-mysql.services.clever-cloud.com',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectModule: mysql2
 });
 
 export default db;
