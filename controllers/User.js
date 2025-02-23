18daf52b1d7577d6ce55d011e484d9c86f0172df
jeje <akhmdjzl17@gmail.com>
2025-02-23 13:50:58 +0700

    tessss

--- controllers/User.js
+++ controllers/User.js
@@ -1,5 +1,5 @@
 import { Users } from "../models/ProductModel.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
  
 export const getUsers = async(req, res) => {
